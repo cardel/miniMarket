f6263df9b2b4f070b8282bf471816d931a17ab79
Joshua <Joshua@Joshua-PC>
2014-10-13 15:32:20 -0500

    Connect

---
+++ src/logica/Connect.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor
+ */
+package logica;
+
+/**
+ *
+ * @author Joshua
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Connect {
+    
+    Connection conn = null;
+    
+    public static Connection connectDB()
+    {
+        try{
+            String sDriverName = "org.sqlite.JDBC";
+            Class.forName(sDriverName);
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:database1.sqlite");
+            JOptionPane.showMessageDialog(null, "Connect");
+            return conn;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+}
