6922fb1e0628bfef8a96cb5c99398cbd17e409e4
cardel <cardel@cardel-PC>
2014-10-19 09:49:09 -0500

    Controlador Cliente

---
+++ src/controladores/ControladorCliente.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import logica.SQLManager;
+import entidades.Cliente;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author cardel
+ */
+public class ControladorCliente {
+
+    private SQLManager sQLManager;
+
+    public ControladorCliente() {
+        sQLManager = new SQLManager();
+    }
+
+    public ArrayList<Cliente> obtenerClientes() {
+        String selection[] = {"cliente_id", "tipo_id_cliente", "nombre", "numero_de_telefono", "numero_celular", "direccion", "monto_prestamo"};
+        String selection_type[] = {"int", "varchar", "varchar", "varchar", "varchar", "varchar", "int"};
+        String table = "Cliente";
+        String restriction = "";
+        String resultado[] = sQLManager.select_query(selection, selection_type, table, restriction);
+        ArrayList<Cliente> listaDeClientes = new ArrayList<>();
+        //Pendiente
+        int cliente_id = Integer.parseInt(resultado[0]);
+        String tipo_cliente_id = resultado[1];
+        String nombre = resultado[1];
+        String numero_telefono = resultado[1];
+        String numero_celular = resultado[1];
+        String direccion = resultado[1];
+        double monto_prestamo = Double.parseDouble(resultado[1]);
+        Cliente cliente = new Cliente(cliente_id, direccion, nombre, numero_telefono, numero_celular, direccion, monto_prestamo);
+        listaDeClientes.add(cliente);
+
+        return listaDeClientes;
+    }
+
+    public boolean editarCliente(int cliente_id, String tipo_cliente_id, String nombre, String numero_telefono, String numero_celular, String direccion, double monto_prestamo) {
+
+        
+        String selection[] = {"cliente_id", "tipo_id_cliente", "nombre", "numero_de_telefono", "numero_celular", "direccion", "monto_prestamo"};
+        String value[] = {String.valueOf(cliente_id), tipo_cliente_id, nombre,numero_telefono,numero_celular,direccion,String.valueOf(monto_prestamo)};
+           String type_value[] = {"int", "varchar", "varchar", "varchar", "varchar", "varchar", "int"};
+
+        String table = "Cliente";
+        String condition = " where cliente_id="+cliente_id;
+        sQLManager.update_query(selection, value, type_value, table, condition);
+        return true;
+
+    }
+
+}
--- src/controladores/ControladorConfiguracionesGlobales.java
+++ src/controladores/ControladorConfiguracionesGlobales.java
@@ -4,29 +4,32 @@
  * and open the template in the editor.
  */
 package controladores;
+
 import logica.SQLManager;
 import entidades.ConfiguracionesGlobales;
+
 /**
  *
  * @author cardel
  */
 public class ControladorConfiguracionesGlobales {
+
     private SQLManager sQLManager;
 
     public ControladorConfiguracionesGlobales() {
         sQLManager = new SQLManager();
     }
-    
-        public ConfiguracionesGlobales obtenerConfiguracionActual() {
+
+    public ConfiguracionesGlobales obtenerConfiguracionActual() {
         String selection[] = {"RazonSocial", "NIT", "NombreAdministrador"};
         String selection_type[] = {"varchar", "varchar", "varchar"};
         String table = "ConfiguracionesGlobales";
         String restriction = "";
         String resultado[] = sQLManager.select_query(selection, selection_type, table, restriction);
 
-        String razonSocial=resultado[0];
-        String NIT=resultado[1];
-        String NombreAdministrador=resultado[2];
+        String razonSocial = resultado[0];
+        String NIT = resultado[1];
+        String NombreAdministrador = resultado[2];
 
         ConfiguracionesGlobales configuracionGlobal = new ConfiguracionesGlobales(razonSocial, NIT, NombreAdministrador);
         return configuracionGlobal;
@@ -41,5 +44,5 @@ public class ControladorConfiguracionesGlobales {
         sQLManager.update_query(selection, value, type_value, table, condition);
         return true;
     }
-    
+
 }
